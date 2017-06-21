07be938d8711c0f7b151eed188ce39091f7c4229
Rakesh Chotalia <Rax@rax-mbp.home>
2017-06-21 21:40:17 +0100

    fix to image

--- app/components/Main.jsx
+++ app/components/Main.jsx
@@ -5,11 +5,11 @@ import Nav from 'Nav'
 export default class Main extends React.Component {
 	render() {
 		return (
-			<div className="container">
+			<div className="container-fluid">
 				<Header/>
 				<Nav/>
 				<div className="row">
-					<div className="col-lg-12 col-md-12 col-sm-12">
+					<div className="col-lg-12 col-md-12 col-sm-12 col-xs-12">
 						{this.props.children}
 					</div>
 				</div>
