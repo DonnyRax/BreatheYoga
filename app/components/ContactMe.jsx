4fb3a7607b190e0427cdfdf40d453ce53fc98a97
Rakesh Chotalia <Rax@rax-mbp.home>
2017-06-23 23:01:19 +0100

    contact me form

--- app/components/ContactMe.jsx
+++ app/components/ContactMe.jsx
@@ -2,8 +2,44 @@ import React from 'react'
 
 var ContactMe = (props) =>  {
     return (
-        <div>
-            Contact Me Page
+        <div className="row">
+            <div className="col-lg-offset-3 col-lg-6" id="contact-form">
+                <form>
+                    <div className="group">
+                        <input type="text" required/>
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label><i className="fa fa-user-o"></i>Name</label>
+                    </div>
+
+                    <div className="group">
+                        <input type="text" required/>
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label><i className="fa fa-at"></i>Email</label>
+                    </div>
+
+                    <div className="group">
+                        <input type="text" required/>
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label><i className="fa fa-phone"></i>Contact No</label>
+                    </div>
+
+                    <div className="group">
+                        <textarea></textarea>
+                        <span className="highlight"></span>
+                        <span className="bar"></span>
+                        <label><i className="fa fa-comment-o"></i>Message</label>
+                    </div>
+                    <div className="group">
+                        <center> 
+                            <button type="submit" className="btn btn-default">Send <span className="glyphicon glyphicon-send"></span></button>
+                        </center>
+                    </div>
+                </form>
+
+            </div>
         </div>
     )
 }
--- app/components/Header.jsx
+++ app/components/Header.jsx
@@ -5,11 +5,9 @@ export default class Header extends React.Component{
 	render() {
 		return (
             <div id="top-header" className="row">
-                <div className="col-lg-2"></div>
-                <div id="logo-container" className="col-xs-12 col-lg-8 center-content">
+                <div id="logo-container" className="col-xs-12 col-lg-offset-2 col-lg-8 center-content">
                     <img id="logo" src="./content/images/breathe-info-white.png" alt="breathe logo" />
                 </div>
-                <div className="col-lg-2"></div>
             </div>                
 		)
 	}
