de6e2c274d6a0893778d4f6c82e1c1e4734f4664
Rakesh Chotalia <rakesh.chotalia@gmail.com>
2017-07-06 23:17:59 +0100

    removed old footer

--- app/components/Footer.jsx
+++ app/components/Footer.jsx
@@ -3,20 +3,6 @@ import React from 'react'
 var Footer = (props) =>  {
     return (
         <div id="footer" className="row">
-        {/*
-            <div className="col-xs-6 visible-xs">
-                Middle
-            </div>
-            <div id="social-media" className="col-xs-6 visible-xs">
-                <div className="pull-right">
-                    <i className="fa fa-2x fa-facebook-square"></i>
-                    <i className="fa fa-2x fa-instagram"></i>
-                </div>
-            </div>
-            <div id="copyright" className="col-xs-12 visible-xs center-content">
-                Copyright &copy; 2017
-            </div>
-        */}
             <div id="footer-left" className="col-xs-4 center-content">
                 Copyright &copy; 2017
             </div>
