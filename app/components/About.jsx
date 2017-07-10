5b34ddf06751b3fe6b3d2869eed73be4108ac13e
Rakesh Chotalia <rakesh.chotalia@gmail.com>
2017-07-10 22:15:32 +0100

    about page

--- app/components/About.jsx
+++ app/components/About.jsx
@@ -3,7 +3,71 @@ import React from 'react'
 var About = (props) =>  {
     return (
         <div>
-            About Page
+            <div className="row">
+                <div className="col-xs-12 visible-xs">
+                    <div className="page-header">
+                        <h3 className="page-header-title">About</h3>
+                    </div>
+                </div>
+            </div>
+            <div className="row">
+                <div className="col-xs-12">
+                    <img id="image-float-right" src="./content/images/yoga.jpg" alt="yoga" />
+                    <p>
+                        I started my journey with yoga 10 years ago when I moved to Thailand to teach English. There were 
+                        many yoga retreats and classes out there, so one day I thought I’d give it a go. My first ever class 
+                        was on a wooden platform surrounded by jungle; it was a stunning location. I didn’t know what I was 
+                        doing and I couldn’t bend into the positions as well as the other more experienced yogis, but it 
+                        didn’t matter. It was the feelings I got from doing it that mattered: calm, focussed, connected to 
+                        nature, energised and happy. I was hooked.
+                    </p>
+                    <p>
+                        From there, I tried a variety of classes in a variety of locations, until I found Mary Verkuyl, an 
+                        incredibly special yoga teacher, who taught me for 4 years. Mary was my guru. She taught me that yoga 
+                        wasn’t just a weekly class, but a way of life. I learnt that the true purpose of practising yoga was to 
+                        train your body to be able to sit in a meditative posture for a long period of time. Practising yoga not 
+                        only prepares the body for meditation, but also focusses the mind. At the end of Mary’s class, we would 
+                        practice meditation and then I would get on my scooter to ride home and I would feel like I was floating, 
+                        like I was invincible and that nothing could affect me.
+                    </p>
+                    <p>
+                        It was from her that I learnt how beneficial yoga could be for the body and the mind. She taught me 
+                        about the healing powers of yoga, using a mixture of postures (asanas), breathing (pranayama) and 
+                        meditation to remove energy blockages within the body that can cause disease.  I started to practice 
+                        these techniques at home when I had a particular ailment. At first, I didn’t believe that they would 
+                        do anything, but time after time, yoga proved me wrong.
+                    </p>
+                    <img id="image-float-left" src="./content/images/yoga3.jpg" alt="yoga" />
+                    <p>
+                        Moving back to England was a shock to the system. I could feel the stress and fast pace of life almost 
+                        as soon as I landed. Teaching at a school back in the UK was 100 times more stressful than teaching in 
+                        Thailand. Luckily, I had made yoga a daily ritual; I practised it every morning for 20-30 minutes before 
+                        breakfast, which would give me focus for the day, and I would meditate in the evening, which would calm 
+                        my busy mind before sleep. During the most stressful times, yoga was the only thing that kept me from 
+                        having a breakdown. The more people I talked to about my struggles with stress and anxiety, the more 
+                        people opened up to me and shared that they were feeling the same or had felt the same in the past. I 
+                        couldn’t believe how many people suffer, often in silence, and how many people’s mental health is affected 
+                        from stress and anxiety. I knew then that I wanted to help in some way.
+                    </p>
+                    <p>
+                        I have seen first-hand how yoga is beneficial, both mentally and physically, and my passion for sharing 
+                        this knowledge has driven me to complete my yoga teaching qualification this year. I have now quit my job 
+                        to set up ‘breathe’ so that I can teach others how yoga can be incorporated into daily life to improve 
+                        their health and wellbeing.
+                    </p>
+                    <p>
+                        Yoga and meditation can:
+                    </p>
+                    <ul className="about-yoga-list">
+                        <li>improve strength, flexibility, posture, focus and balance</li>
+                        <li>reduce stress and tension</li>
+                        <li>increase awareness of your body and mind</li>
+                        <li>improve your digestive system and your nervous system</li>
+                        <li>increase circulation</li>
+                        <li>unblock energy knots</li>
+                    </ul>
+                </div>
+            </div>
         </div>
     )
 }
--- app/components/Testimonials.jsx
+++ app/components/Testimonials.jsx
@@ -4,12 +4,12 @@ var Testimonials = (props) =>  {
     return (
         <div>
             <div className="row">
-                    <div className="col-xs-12 visible-xs">
-                        <div className="page-header">
-                            <h3 className="page-header-title">Testimonials</h3>
-                        </div>
+                <div className="col-xs-12 visible-xs">
+                    <div className="page-header">
+                        <h3 className="page-header-title">Testimonials</h3>
                     </div>
                 </div>
+            </div>
             <div className="row">
                 <div className="col-xs-10 col-xs-offset-1 col-md-4 col-md-offset-0 testimonial-container">
                     <p className="testimonial testimonial-date">
