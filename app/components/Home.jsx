1022c464fbb98b2982789458d26191d48cc67253
Rakesh Chotalia <rakesh.chotalia@gmail.com>
2017-07-11 22:22:33 +0100

    image responsive

--- app/components/Home.jsx
+++ app/components/Home.jsx
@@ -8,13 +8,13 @@ var Home = (props) =>  {
                 <div className="hidden-xs col-md-12 strapline-text center-content">
                     Create space and time to be you
                 </div>
-                <div className="ol-xs-12 visible-xs strapline-text text-small center-content">
+                <div className="col-xs-12 visible-xs strapline-text text-small center-content">
                     Create space and time to be you
                 </div>
             </div>
             <div className="row">
                 <div id="home-image-container" className="col-xs-12 col-md-offset-2 col-md-8 center-content">
-                    <img id="yoga-image" src="./content/images/yoga2.jpg" alt="yoga"/>
+                    <img id="yoga-image" className="img-responsive" src="./content/images/yoga2.jpg" alt="yoga"/>
                 </div>
             </div>
 			<Footer/>
